65b588254248ca85f6cac49cd32fce4626510276
lnatkamol <lnatkamol@Hotmail.com>
2019-01-07 17:18:55 -0800

    match history update

--- history.php
+++ history.php
@@ -41,62 +41,27 @@ else{
     mysqli_set_charset($link, 'utf8');
     $sql = "USE wood_pingpong";
     $result = mysqli_query($link, $sql);
-    $sql = "SELECT rating FROM player WHERE name = '$winner' ";
+    $sql = "SELECT * FROM $pname";
     $result = mysqli_query($link, $sql);
-    $ratingW = mysqli_fetch_assoc($result);
-    $sql = "SELECT rating FROM player WHERE name = '$loser' ";
-    $result = mysqli_query($link, $sql);
-    $ratingL = mysqli_fetch_assoc($result);
-    echo "$winner"."'s rating was ". $ratingW['rating'] ."<br>";
-    echo "$loser"."'s rating was ". $ratingL['rating'] ."<br>";
-    echo "<br>";
- 
-    $QA = pow(10,($ratingW['rating'] / 400));
-    $QB = pow(10,($ratingL['rating'] / 400));
-    $EA = $QA/($QA+$QB);
-    $EB = $QB/($QA+$QB); 
-
-    echo "$winner"."'s probability of winning was ". $EA*100 ."%<br>";
-    echo "$loser"."'s probability of winning was ". $EB*100 ."%<br>";
-    echo "<br>";
-    
-    $newratingW = $ratingW['rating'] + (30*(1-$EA));
-    $newratingL = $ratingL['rating'] + (30*(0-$EB));
-    
-    echo "$winner"."'s NEW rating is ". $newratingW ."<br>";
-    echo "$loser"."'s NEW rating is ". $newratingL ."<br>";
-    echo "<br>";    
-    
-    $sql = "UPDATE player SET wins = wins+1, rating = $newratingW WHERE name = '$winner' ";
-    $result = mysqli_query($link, $sql);
-    if($result){
-        echo "Winner's ELO updated successfully!<br>";
+    echo "<h2> $pname"."'s Match History </h2>";
+    if(mysqli_num_rows($result) > 0){
+        echo "<table border=1>
+            <tr>
+            <th>Game</th>
+            <th>Time</th>
+            <th>Won against</th>
+            <th>Lost to</th>
+            </tr>";
+        while($row = mysqli_fetch_assoc($result)){
+            echo "<tr><td>$row[game_number]</td><td> $row[time_played]</td><td>$row[won_against]</td><td>$row[lost_to]</td></tr>"; 
+        }
+        echo "</table>";
     }else{
-        echo "Unable to update ELO<br>";
-    }
-    $sql = "UPDATE player SET loses = loses+1, rating = $newratingL WHERE name = '$loser' ";
-    $result = mysqli_query($link, $sql);
-    if($result){
-        echo "Loser's ELO updated successfully!<br>";
-    }else{
-        echo "Unable to update ELO<br>";
+        echo "No data";
     }
  
-    $time = date("Y-m-d H:i:s");
-    $sql = "INSERT into $winner (time_played, won_against) VALUES ('$time', '$loser')";
-    $result = mysqli_query($link, $sql);
-    if(!$result){
-        die('error'.mysqli_error());
-    }
-    $sql = "INSERT into $loser (time_played, lost_to) VALUES ('$time', '$winner')";
-    $result = mysqli_query($link, $sql);
-    if(!$result){
-        die('error'.mysqli_error());
-    }
-    
     mysqli_close($link);
     
-    echo '<a href="ranking.php">View Ranking</a>';
     echo '<br><a href=index.html>Go back to main page</a>';
 }
 ?>
